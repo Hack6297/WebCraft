e65e44813b411eaf44a1e3f521b0a56fc9237c54
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2025-05-20 21:50:50 -0700

    Delete webcraft_django_fixed directory

--- webcraft_django_fixed/webcraft/settings.py
+++
@@ -1,9 +0,0 @@
-
-SECRET_KEY = 'example'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
-INSTALLED_APPS = ['channels', 'django.contrib.staticfiles']
-ASGI_APPLICATION = 'server.application'
-ROOT_URLCONF = 'webcraft.urls'
-MIDDLEWARE = []
-DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': 'db.sqlite3'}}
