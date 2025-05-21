1a8aa1a899932d3ae454770ff8e74676ddc751bc
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2025-05-20 21:53:46 -0700

    Delete asgi.py

--- asgi.py
+++
@@ -1,9 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebCraft.settings')
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-})
