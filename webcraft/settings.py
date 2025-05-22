aeb703b57aa446c7decab7e4186f21fcd95923dc
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2025-05-21 20:42:10 -0700

    Update settings.py

--- webcraft/settings.py
+++ webcraft/settings.py
@@ -1,13 +1,13 @@
-SECRET_KEY = 'django-insecure-webcraft'
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'your-secret-key'  # Заменить на безопасный ключ
+
 DEBUG = True
-ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS = [
-    'channels',
-    'django.contrib.staticfiles',
-]
-
-ASGI_APPLICATION = "webcraft.asgi.application"
+ALLOWED_HOSTS = ['*']  # Разрешаем всё, можно ограничить под домен
 
 INSTALLED_APPS = [
     'channels',
@@ -19,8 +19,57 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
-# Redis или InMemoryChannelLayer для локального теста
+ROOT_URLCONF = 'WebCraft.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'WebCraft.wsgi.application'
+ASGI_APPLICATION = 'WebCraft.asgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer"
